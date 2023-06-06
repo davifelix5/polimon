784f60631c73577fea984bf67209b60cb132935c
Davi Felix <davifelix@usp.br>
2023-06-06 19:16:37 -0300

    Melhorando desempenho ao carregar imagens.

--- src/game/state/Menu.java
+++ src/game/state/Menu.java
@@ -1,6 +1,5 @@
 package game.state;
 
-import game.Game;
 import game.buttons.*;
 import game.buttons.Button;
 import game.handlers.MouseHandler;
@@ -14,13 +13,14 @@ import java.io.IOException;
 public class Menu implements IState{
 
 	Button play, options, credits, exit;
-
 	MouseHandler mouse;
 	IStateManager stateManager;
+	BufferedImage backgroundImage;
 
 	public Menu(MouseHandler mouse, IStateManager stateManager) {
 		this.mouse = mouse;
 		this.stateManager = stateManager;
+		loadImages();
 	}
 
 	public void tick() {
@@ -29,28 +29,30 @@ public class Menu implements IState{
 
 	public void render(Graphics g) {
 
-		try{
-			BufferedImage bg = ImageIO.read(new FileInputStream("src/game/res/fotos/imeBlurred.jpg"));
-			g.drawImage(bg, 0, 0, null);
-			play = new Button("Play", 180, 150, 600, 80, mouse, new PlayButtonStrategy());
-			options = new Button("Options", 180, 250, 600, 80, mouse, new OptionsButtonStrategy());
-			credits = new Button("Credits", 180, 350, 600, 80, mouse, new CreditsButtonStrategy());
-			exit = new Button("Exit", 180, 450, 600, 80, mouse, new ExitButtonStrategy());
+		g.drawImage(backgroundImage, 0, 0, null);
+		play = new Button("Play", 180, 150, 600, 80, mouse, new PlayButtonStrategy());
+		options = new Button("Options", 180, 250, 600, 80, mouse, new OptionsButtonStrategy());
+		credits = new Button("Credits", 180, 350, 600, 80, mouse, new CreditsButtonStrategy());
+		exit = new Button("Exit", 180, 450, 600, 80, mouse, new ExitButtonStrategy());
 
-			Font h1 = new Font("arial", Font.BOLD, 48);
+		Font h1 = new Font("arial", Font.BOLD, 48);
 
-			g.setFont(h1);
-			g.setColor(Color.white);
-			g.drawString("Main Menu", (960 - (9 * h1.getSize() / 2)) / 2, 100);
+		g.setFont(h1);
+		g.setColor(Color.white);
+		g.drawString("Main Menu", (960 - (9 * h1.getSize() / 2)) / 2, 100);
 
-			play.render(g);
-			options.render(g);
-			credits.render(g);
-			exit.render(g);
-		}catch(IOException e){
+		play.render(g);
+		options.render(g);
+		credits.render(g);
+		exit.render(g);
+	}
+
+	public void loadImages() {
+		try {
+			this.backgroundImage = ImageIO.read(new FileInputStream("src/game/res/fotos/imeBlurred.jpg"));
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	@Override
--- src/game/state/RestScreen.java
+++ src/game/state/RestScreen.java
@@ -7,20 +7,20 @@ import javax.imageio.ImageIO;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.event.KeyAdapter;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class RestScreen extends KeyAdapter implements IState {
+public class RestScreen implements IState {
 
 	KeyHandler keyHandler;
 	IStateManager stateManager;
+	private BufferedImage backgroundImage;
 
 	public RestScreen(KeyHandler keyHandler, IStateManager stateManager) {
 		this.stateManager = stateManager;
 		this.keyHandler = keyHandler;
-
+		loadImages();
 	}
 
 	public void tick() {
@@ -29,29 +29,31 @@ public class RestScreen extends KeyAdapter implements IState {
 		}
 	}
 	public void render(Graphics g) {
+
+		g.drawImage(backgroundImage, 0, 0, null);
+
+		Font t1 = Fontes.getPokemonSolid();
+		Font t2 = Fontes.getPokemonHollow();
+		Font subTitle = new Font("arial", Font.BOLD, 32);
+
+		g.setColor(Color.YELLOW);
+		g.setFont(t1);
+		g.drawString("Polimon", (960 - (8 * t1.getSize() / 2)) / 2, 250);
+		g.setColor(Color.BLUE);
+		g.setFont(t2);
+		g.drawString("Polimon", (960 - (8 * t2.getSize() / 2)) / 2, 250);
+		g.setColor(Color.WHITE);
+		g.setFont(subTitle);
+		g.drawString("Pressione qualquer tecla...", (960 - (25 * subTitle.getSize() / 2)) / 2, 400);
+	}
+
+	public void loadImages() {
 		try {
-
-			BufferedImage bg = ImageIO.read(new FileInputStream("src/game/res/fotos/bienioBlurred.jpg"));
-			g.drawImage(bg, 0, 0, null);
-
-			Font t1 = Fontes.getPokemonSolid();
-			Font t2 = Fontes.getPokemonHollow();
-			Font subTitle = new Font("arial", Font.BOLD, 32);
-
-			g.setColor(Color.YELLOW);
-			g.setFont(t1);
-			g.drawString("Polimon", (960 - (8 * t1.getSize() / 2)) / 2, 250);
-			g.setColor(Color.BLUE);
-			g.setFont(t2);
-			g.drawString("Polimon", (960 - (8 * t2.getSize() / 2)) / 2, 250);
-			g.setColor(Color.WHITE);
-			g.setFont(subTitle);
-			g.drawString("Pressione qualquer tecla...", (960 - (25 * subTitle.getSize() / 2)) / 2, 400);
+			this.backgroundImage = ImageIO.read(new FileInputStream("src/game/res/fotos/bienioBlurred.jpg"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
 
 	@Override
 	public IStateManager getStateManager() {
