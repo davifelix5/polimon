e6cbae714592e2930175ec46d263111b58df2676
unknown <pedrohenrique2001@gmail.com>
2023-06-06 17:31:28 -0300

    adicionado Fonte nova no menu

--- src/game/state/RestScreen.java
+++ src/game/state/RestScreen.java
@@ -1,11 +1,16 @@
 package game.state;
 
 import game.GamePanel;
+import game.utilities.Fontes;
 
+import javax.imageio.ImageIO;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyAdapter;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class RestScreen extends KeyAdapter {
 
@@ -13,17 +18,30 @@ public class RestScreen extends KeyAdapter {
 		
 	}
 	public void render(Graphics g) {
-		
-		Font h1 = new Font("arial", Font.BOLD, 75);
-		Font h2 = new Font("arial", Font.BOLD, 32);
-		g.setColor(Color.BLACK);
-		g.fillRect(0,0, GamePanel.width, GamePanel.height);
+		try{
 
-		g.setColor(Color.WHITE);
-		g.setFont(h1);
-		g.drawString("Polimon", (960 - (8 * h1.getSize() / 2)) / 2, 250);
-		g.setFont(h2);
-		g.drawString("Pressione qualquer tecla...", (960 - (25 * h2.getSize() / 2)) / 2, 400);
+			BufferedImage bg = ImageIO.read(new FileInputStream("src/game/res/fotos/bienioBlurred.jpg"));
+			g.drawImage(bg, 0, 0, null);
+
+			Font t1 = Fontes.getPokemonSolid();
+			Font t2 = Fontes.getPokemonHollow();
+			Font subTitle = new Font("arial", Font.BOLD, 32);
+			g.setColor(Color.BLACK);
+			g.fillRect(0,0, GamePanel.width, GamePanel.height);
+
+			g.setColor(Color.YELLOW);
+			g.setFont(t1);
+			g.drawString("Polimon", (960 - (8 * t1.getSize() / 2)) / 2, 250);
+			g.setColor(Color.BLUE);
+			g.setFont(t2);
+			g.drawString("Polimon", (960 - (8 * t2.getSize() / 2)) / 2, 250);
+			g.setColor(Color.WHITE);
+			g.setFont(subTitle);
+			g.drawString("Pressione qualquer tecla...", (960 - (25 * subTitle.getSize() / 2)) / 2, 400);
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+
 	
 
 		
