69dfff2b32d8c3848717d3b2eaa8383bf1a7e459
Davi <davifelix@usp.br>
2023-06-15 09:03:40 -0300

    correact resource names

--- src/game/pokemon/Pokemon.java
+++ src/game/pokemon/Pokemon.java
@@ -2,13 +2,13 @@ package game.pokemon;
 
 public class Pokemon {
     PokemonID ID;
-    final int baseHP;
-    final int baseAttack;
-    final int baseDefence;
-    final int baseSpAtk;
-    final int baseSpDef;
-    final int baseSpeed;
-    final PokemonType type;
+    final int baseHP = 2;
+    final int baseAttack = 2;
+    final int baseDefence = 2;
+    final int baseSpAtk = 2;
+    final int baseSpDef = 2;
+    final int baseSpeed = 2;
+    final PokemonType type = PokemonType.Normal;
     PokemonID evolvesTo;
 
     public Pokemon(PokemonID ID) {
